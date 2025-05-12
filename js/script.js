4b91eef712f6761c7f4e9ca3e4dec0b0ef87c713
3-126-2-PC <you@example.com>
2025-05-12 12:55:46 +1200

    12.05.25 практика

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 //#region Глобальные переменные
-const HOST = 'http://api-messenger.web-srv.local';
+const host = 'http://api-messenger.web-srv.local ';
 const CONTENT = document.querySelector('.content');
 //#endregion
+
+var token = ''
 
 //#region AJAX запрос
 function _get(params, callback) {
     let get = new XMLHttpRequest();
-
     get.open('GET', `${params.url}`);
+    get.setRequestHeader( "Authorization", "bearer" + token )
     get.send();
     get.onreadystatechange = function () {
         if (get.readyState == 4) {
@@ -18,6 +20,7 @@ function _get(params, callback) {
 function _post(params, callback) {
     let post = new XMLHttpRequest();
     post.open('POST', `${params.url}`);
+    post.setRequestHeader( "Authorization", "bearer" + token )
     post.send(params.data);
     post.onreadystatechange = function () {
         if (post.readyState == 4) {
@@ -32,39 +35,59 @@ function _elem(selector) {
     return document.querySelector(selector)
 }
 
-_get({ url: 'modules/auth.html' }, function (response) {
-    CONTENT.innerHTML = response;
+var get_auth = new XMLHttpRequest();
+get_auth.open('GET', 'modules/auth.html');
+get_auth.send()
+get_auth.onreadystatechange = function() {
+    if (get_auth.readyState == 4) {
+        CONTENT.innerHTML = this.responseText
 
-    onLoadAuth()
-})
+        onLoadAuth()
+    }
+}
 
 function onLoadAuth() {
     _elem('.authorize').addEventListener('click', function() {
         let request_auth = new FormData();
         request_auth.append('email', _elem('input[name="email"]').value);
-        request_auth.append('password', _elem('input[name="password'))
-    });
-    onLoadRegistration();
-}
-function onLoadRegistration() {
-    _elem('.regist').addEventListener('click', function() {
-        _get({url: 'modules/regist.html'}, function(response) {
-            CONTENT.innerHTML = response;
-            onLoadReg()
+        request_auth.append('pass', _elem('input[name="password"]').value);
+
+        _post({url: `${host}/auth/`, data: request_auth}, function(response) {
+        response = JSON.parse(response);
+        console.log(response);
+
+        if(response.success) {
+            _get({ url: '/modules/main.html' }, function (response) {
+                CONTENT.innerHTML = response;
+                onLoadMain()
+                });
+            };
         });
+    });
+    _elem('.regist').addEventListener('click', function() {
+    var get_reg = new XMLHttpRequest();
+    get_reg.open('GET', 'modules/regist.html');
+    get_reg.send()
+    get_reg.onreadystatechange = function() {
+        if (get_reg.readyState == 4) {
+            CONTENT.innerHTML = this.responseText
+
+            onLoadReg()
+        }
+    }
     });
 }
 
 function onLoadReg() {
     _elem('.go-reg').addEventListener('click', function() {
-        let request_reg = new XMLHttpRequest();
+        let request_reg = new FormData();
         request_reg.append('email', _elem('input[name="email"]').value);
         request_reg.append('pass', _elem('input[name="password"]').value);
-        request_reg.append('fam', _elem('input[name="last-name"]').value);
-        request_reg.append('name', _elem('input[name="first-name"]').value);
+        request_reg.append('fam', _elem('input[name="fam"]').value);
+        request_reg.append('name', _elem('input[name="name"]').value);
         request_reg.append('otch', _elem('input[name="otch"]').value);
 
-        _post({url: `${HOST}/user`, data: request_reg}, function(response) {
+        _post({url: `${host}/user`, data: request_reg}, function(response) {
             response = JSON.parse(response);
             console.log(response);
 
