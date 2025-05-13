c961ccf391c29479638225f4953f696845748c62
3-126-2-PC <you@example.com>
2025-05-13 12:56:30 +1200

    13.05.25 практика

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 //#region Глобальные переменные
-const host = 'http://api-messenger.web-srv.local ';
+const host = 'http://api-messenger.web-srv.local';
 const CONTENT = document.querySelector('.content');
 //#endregion
 
@@ -9,7 +9,7 @@ var token = ''
 function _get(params, callback) {
     let get = new XMLHttpRequest();
     get.open('GET', `${params.url}`);
-    get.setRequestHeader( "Authorization", "bearer" + token )
+    get.setRequestHeader("Authorization", "bearer" + token)
     get.send();
     get.onreadystatechange = function () {
         if (get.readyState == 4) {
@@ -20,7 +20,7 @@ function _get(params, callback) {
 function _post(params, callback) {
     let post = new XMLHttpRequest();
     post.open('POST', `${params.url}`);
-    post.setRequestHeader( "Authorization", "bearer" + token )
+    post.setRequestHeader("Authorization", "bearer" + token)
     post.send(params.data);
     post.onreadystatechange = function () {
         if (post.readyState == 4) {
@@ -35,46 +35,62 @@ function _elem(selector) {
     return document.querySelector(selector)
 }
 
-var get_auth = new XMLHttpRequest();
-get_auth.open('GET', 'modules/auth.html');
-get_auth.send()
-get_auth.onreadystatechange = function() {
-    if (get_auth.readyState == 4) {
+var get_choise = new XMLHttpRequest();
+get_choise.open('GET', 'modules/choise.html');
+get_choise.send()
+get_choise.onreadystatechange = function() {
+    if (get_choise.readyState == 4) {
         CONTENT.innerHTML = this.responseText
 
-        onLoadAuth()
+        onLoadAuthorize(), onLoadRegist()
     }
+}
+
+function onLoadAuthorize() {
+    _elem('.choise-auth').addEventListener('click', function() {
+        let get_auth = new XMLHttpRequest();
+        get_auth.open('GET', 'modules/auth.html');
+        get_auth.send()
+        get_auth.onreadystatechange = function() {
+            if (get_auth.readyState == 4) {
+                CONTENT.innerHTML = this.responseText
+
+                onLoadAuth()
+            }
+        }
+    });
 }
 
 function onLoadAuth() {
     _elem('.authorize').addEventListener('click', function() {
         let request_auth = new FormData();
         request_auth.append('email', _elem('input[name="email"]').value);
-        request_auth.append('pass', _elem('input[name="password"]').value);
+        request_auth.append('pass', _elem('input[name="pass"]').value);
 
-        _post({url: `${host}/auth/`, data: request_auth}, function(response) {
-        response = JSON.parse(response);
-        console.log(response);
+        _post({url: `${host}/auth`, data: request_auth}, function(response) {
+            response = JSON.parse(response);
+            console.log(response);
 
-        if(response.success) {
-            _get({ url: '/modules/main.html' }, function (response) {
-                CONTENT.innerHTML = response;
+            _get({url: 'modules/main.html'}, function(response) {
+                CONTENT.innerHTML = response
                 onLoadMain()
-                });
-            };
-        });
-    });
-    _elem('.regist').addEventListener('click', function() {
-    var get_reg = new XMLHttpRequest();
-    get_reg.open('GET', 'modules/regist.html');
-    get_reg.send()
-    get_reg.onreadystatechange = function() {
-        if (get_reg.readyState == 4) {
-            CONTENT.innerHTML = this.responseText
+            })
+        })
+    })
+}
 
-            onLoadReg()
+function onLoadRegist() {
+    _elem('.choise-regist').addEventListener('click', function() {
+        let get_regist = new XMLHttpRequest();
+        get_regist.open('GET', 'modules/regist.html');
+        get_regist.send()
+        get_regist.onreadystatechange = function() {
+            if (get_regist.readyState == 4) {
+                CONTENT.innerHTML = this.responseText
+
+                onLoadReg()
+            }
         }
-    }
     });
 }
 
@@ -82,7 +98,7 @@ function onLoadReg() {
     _elem('.go-reg').addEventListener('click', function() {
         let request_reg = new FormData();
         request_reg.append('email', _elem('input[name="email"]').value);
-        request_reg.append('pass', _elem('input[name="password"]').value);
+        request_reg.append('pass', _elem('input[name="pass"]').value);
         request_reg.append('fam', _elem('input[name="fam"]').value);
         request_reg.append('name', _elem('input[name="name"]').value);
         request_reg.append('otch', _elem('input[name="otch"]').value);
@@ -92,12 +108,17 @@ function onLoadReg() {
             console.log(response);
 
             if(response.success) {
-                _get({ url: '/modules/main.html' }, function (response) {
-                    CONTENT.innerHTML = response;
+                _get({url: 'modules/main.html'}, function(response) {
+                    CONTENT.innerHTML = response
                     onLoadMain()
-                });
-            };
-        });
-    });
+                })
+            }
+        })
+    })
+}
+
+function onLoadMain() {
+    _elem('.chats').addEventListener('click', function() {
+    })
 }
 //#endregion
